ad6d20f85aab28b1034af225b514787b68098cd2
장원식 <wonsik@Ethan-MacBookPro.local>
2023-04-29 23:52:12 +0900

    db 연결 변경

--- core/db.py
+++ core/db.py
@@ -5,8 +5,7 @@ from os import environ
 #from dotenv.main import load_dotenv
 
 #load_dotenv()
-
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:7569@svc.sel4.cloudtype.app:30322/bewithyou"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:7569@svc.sel4.cloudtype.app:31144/codewarrior"
 
 # format : "사용하는db:/{username}:{password}@{host}:{port}/{db_name}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
